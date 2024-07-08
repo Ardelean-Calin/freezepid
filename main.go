9c90f06bc9e701cf0ba43340a8394ac3afba90a1
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2024-07-08 10:07:13 +0300

    Added timeout

--- main.go
+++ main.go
@@ -3,51 +3,53 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
+	"time"
 )
 
 func main() {
 
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s <pid>\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "\nPositional Arguments:\n")
-		fmt.Fprintf(os.Stderr, "  pid\tPID to try to freeze\n")
-		flag.PrintDefaults()
-	}
+	pid := flag.Int("pid", 1, "PID to try to freeze")
+	timeout := flag.Duration("timeout", time.Minute, "Duration to wait with process hang-up [default: 1 minute]")
 
 	flag.Parse()
-	if flag.NArg() == 0 {
-		flag.Usage()
-		os.Exit(1)
-	}
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	done := make(chan bool, 1)
+	errChan := make(chan error, 1)
 
-	pid, err := strconv.Atoi(flag.Arg(0))
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Freezing PID %d\n", pid)
+	fmt.Printf("Freezing PID %d\n", *pid)
 
 	go func() {
-		err := syscall.PtraceAttach(pid)
+		err := syscall.PtraceAttach(*pid)
 		if err != nil {
-			panic(err)
+			errChan <- fmt.Errorf("PtraceAttach failed: %v", err)
+			return
 		}
+		syscall.PtraceCont(*pid, int(syscall.SIGINT))
+		fmt.Println("Process frozen successfully")
 
-		<-sigs
-		done <- true
+		// Exit if SIGINT or if a minute passed
+		select {
+		case <-sigs:
+			done <- true
+		case <-time.After(*timeout):
+			done <- true
+		}
 	}()
-	<-done
+	select {
+	case <-done:
+	case err := <-errChan:
+		log.Fatalf("Error: %v", err)
+	}
 
-	err = syscall.PtraceDetach(pid)
+	err := syscall.PtraceDetach(*pid)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("exiting")
+	fmt.Println("Process unfrozen successfully")
 }
